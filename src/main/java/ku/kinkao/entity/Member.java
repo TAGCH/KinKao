68dc3ed154e74f30f8d7580f379a20cf0eee4191
TAGCH <pisit.au@ku.th>
2024-11-29 12:51:23 +0700

    Finish KinKao Project

---
+++ src/main/java/ku/kinkao/entity/Member.java
@@ -0,0 +1,38 @@
+package ku.kinkao.entity;
+
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import ku.kinkao.config.AttributeEncryptor;
+import lombok.Data;
+import java.util.UUID;
+import java.time.Instant;
+
+
+@Data
+@Entity
+public class Member {
+
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Convert(converter = AttributeEncryptor.class)
+    private String username;
+
+    private String password;
+
+    @Convert(converter = AttributeEncryptor.class)
+    private String firstName;
+
+    @Convert(converter = AttributeEncryptor.class)
+    private String lastName;
+
+    @Convert(converter = AttributeEncryptor.class)
+    private String email;
+    private Instant createdAt;
+    private String role;
+
+}
---
+++ src/main/java/ku/kinkao/service/SignupService.java
@@ -0,0 +1,44 @@
+package ku.kinkao.service;
+
+import ku.kinkao.entity.Member;
+import ku.kinkao.repository.MemberRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import ku.kinkao.dto.SignupRequest;
+import org.modelmapper.ModelMapper;
+import java.time.Instant;
+
+
+@Service
+public class SignupService {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private MemberRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public boolean isUsernameAvailable(String username) {
+        return repository.findByUsername(username) == null;
+    }
+
+    public void createMember(SignupRequest dto) {
+        Member dao = modelMapper.map(dto, Member.class);
+        dao.setCreatedAt(Instant.now());
+        dao.setRole("ROLE_USER");
+
+        String hashedPassword = passwordEncoder.encode(dto.getPassword());
+
+        dao.setPassword(hashedPassword);
+
+        repository.save(dao);
+    }
+
+    public Member getMember(String username) {
+        return repository.findByUsername(username);
+    }
+}
